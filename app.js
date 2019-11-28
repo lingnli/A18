261cc9f6dfa6633e6279e7de250c4fb175e242f9
lingnili <wmdlifecoba@gmail.com>
2019-11-29 00:18:33 +0800

    Add CRUD routes including homepage,new,edit and delete

--- app.js
+++ app.js
@@ -17,10 +17,32 @@ db.once("open", () => {
 });
 
 //透過mongoose連線database後載入Account Model
-const Account = require("./models/account.js");
+const Record = require("./models/record.js");
 
+//route
 app.get("/", (req, res) => {
-  res.send("this will be an account book app");
+  res.send("首頁：瀏覽所有資料");
+});
+app.get("/record", (req, res) => {
+  res.redirect("/");
+});
+//新增頁面
+app.get("/record/new", (req, res) => {
+  res.send("新增一筆消費頁面");
+});
+app.post("/record/new", (req, res) => {
+  res.send("新增頁面送出動作");
+});
+//編輯頁面
+app.get("/record/edit", (req, res) => {
+  res.send("編輯一筆消費頁面");
+});
+app.post("/record/edit", (req, res) => {
+  res.send("編輯頁面送出動作");
+});
+//刪除
+app.post("/record/delete", (req, res) => {
+  res.send("刪除一筆資料動作");
 });
 
 app.listen(3000, () => {
--- models/record.js
+++ models/record.js
@@ -2,11 +2,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const accountSchema = new Schema({
+const recordSchema = new Schema({
   name: {
     type: String,
     required: true
   }
 });
 
-module.exports = mongoose.model("Account", accountSchema);
+module.exports = mongoose.model("Record", recordSchema);
