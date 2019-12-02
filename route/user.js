795a89fb21b26b782198be45a4dcaa4dfee956fe
lingnili <wmdlifecoba@gmail.com>
2019-12-02 18:00:25 +0800

    Finish register feature

--- route/user.js
+++ route/user.js
@@ -13,8 +13,25 @@ router.post("/login", (req, res) => {
 router.get("/register", (req, res) => {
   res.render("register");
 });
-router.get("/register", (req, res) => {
-  res.render("register");
+router.post("/register", (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  User.findOne({ email: email }).then(user => {
+    if (user) {
+      //email存在
+      console.log("email already registered!");
+      res.render("/register", { name, email });
+    } else {
+      const newUser = new User({ name, email, password });
+      newUser
+        .save()
+        .then(user => {
+          res.render("login"); //新增完成到登入頁面
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  });
 });
 //logout
 router.get("/logout", (req, res) => {
